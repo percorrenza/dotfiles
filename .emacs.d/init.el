751c67d736d65aa16b5ce399875e6771c045a7ff
Tsuyoshi Kindo <tsuyoshi@example.com>
2015-05-06 17:09:18 +0900

    First commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,263 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 3.2 Emacsの起動と終了                                  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; P30 デバッグモードでの起動
+;; おまじない
+(require 'cl)
+;; Emacsからの質問をy/nで回答する
+;; (fset 'yes-or-no-p 'y-or-n-p)
+;; スタートアップメッセージを非表示
+(setq inhibit-startup-screen t)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 4.1 効率的な設定ファイルの作り方と管理方法             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P60-61 Elisp配置用のディレクトリを作成
+;; Emacs 23より前のバージョンを利用している方は
+;; user-emacs-directory変数が未定義のため次の設定を追加
+(when (< emacs-major-version 23)
+  (defvar user-emacs-directory "~/.emacs.d/"))
+
+;; load-path を追加する関数を定義
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+              (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+
+;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
+(add-to-load-path "elisp" "conf" "public_repos")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.2 キーバインドの設定                                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P80 C-hをバックスペースにする
+;; 入力されるキーシーケンスを置き換える
+;; ?\C-?はDELのキーシケンス
+;; (keyboard-translate ?\C-h ?\C-?)
+;;(define-key global-map (kbd "C-x ?") 'help-command)
+
+;;; P79-81 お勧めのキー操作
+;; C-mにnewline-and-indentを割り当てる。
+;; 先ほどとは異なりglobal-set-keyを利用
+(global-set-key (kbd "C-m") 'newline-and-indent)
+;; 折り返しトグルコマンド
+(define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
+;; "C-t" でウィンドウを切り替える。初期値はtranspose-chars
+(define-key global-map (kbd "C-t") 'other-window)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.3 環境変数の設定                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P82-83 パスの設定
+(add-to-list 'exec-path "/opt/local/bin")
+(add-to-list 'exec-path "/usr/local/bin")
+(add-to-list 'exec-path "~/bin")
+
+;;; P85 文字コードを指定する
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.4 フレームに関する設定                               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P87-89 モードラインに関する設定
+;; カラム番号も表示
+;;(column-number-mode t)
+;; ファイルサイズを表示
+(size-indication-mode t)
+;; 時計を表示（好みに応じてフォーマットを変更可能）
+(setq display-time-day-and-date t) ; 曜日・月・日を表示
+;; (setq display-time-24hr-format t) ; 24時表示
+(display-time-mode t)
+
+;;; P90 タイトルバーにファイルのフルパスを表示
+(setq frame-title-format "%f")
+;; 行番号を常に表示する
+(global-linum-mode t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 5.6 表示・装飾に関する設定                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; P95-96 フェイス
+;; リージョンの背景色を変更
+;; (set-face-background 'region "darkgreen")
+
+;; ▼要拡張機能インストール▼
+;;; P96-97 表示テーマの設定
+;; http://download.savannah.gnu.org/releases/color-theme/color-theme-6.6.0.tar.gz
+(when (require 'color-theme nil t)
+  ;; テーマを読み込むための設定
+  (color-theme-initialize)
+  ;; テーマhoberに変更する
+;;(color-theme-hober))
+  (color-theme-deep-blue))
+
+;;; P97-99 フォントの設定
+(when (eq window-system 'ns)
+  ;; asciiフォントをMenloに
+  (set-face-attribute 'default nil
+                      :family "Menlo"
+                      :height 120)
+  ;; 日本語フォントをヒラギノ明朝 Proに
+  (set-fontset-font
+   nil 'japanese-jisx0208
+   ;; 英語名の場合
+   ;; (font-spec :family "Hiragino Mincho Pro"))
+   (font-spec :family "ヒラギノ明朝 Pro"))
+  ;; ひらがなとカタカナをモトヤシーダに
+  ;; U+3000-303F	CJKの記号および句読点
+  ;; U+3040-309F	ひらがな
+  ;; U+30A0-30FF	カタカナ
+  (set-fontset-font
+   nil '(#x3040 . #x30ff)
+   (font-spec :family "NfMotoyaCedar"))
+  ;; フォントの横幅を調節する
+  (setq face-font-rescale-alist
+        '((".*Menlo.*" . 1.0)
+          (".*Hiragino_Mincho_Pro.*" . 1.2)
+          (".*nfmotoyacedar-bold.*" . 1.2)
+          (".*nfmotoyacedar-medium.*" . 1.2)
+          ("-cdac$" . 1.3))))
+
+(when (eq system-type 'windows-nt)
+  ;; asciiフォントをConsolasに
+  (set-face-attribute 'default nil
+                      :family "Consolas"
+                      :height 120)
+  ;; 日本語フォントをメイリオに
+  (set-fontset-font
+   nil
+   'japanese-jisx0208
+   (font-spec :family "メイリオ"))
+  ;; フォントの横幅を調節する
+  (setq face-font-rescale-alist
+        '((".*Consolas.*" . 1.0)
+          (".*メイリオ.*" . 1.15)
+          ("-cdac$" . 1.3))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 6.1 Elispをインストールしよう                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ▼要拡張機能インストール▼
+;;; P113 拡張機能を自動インストール──auto-install
+;; auto-installの設定
+(when (require 'auto-install nil t)	; ←1●
+  ;; 2●インストールディレクトリを設定する 初期値は ~/.emacs.d/auto-install/
+  (setq auto-install-directory "~/.emacs.d/elisp/")
+  ;; EmacsWikiに登録されているelisp の名前を取得する
+  (auto-install-update-emacswiki-package-name t)
+  ;; 必要であればプロキシの設定を行う
+  ;; (setq url-proxy-services '(("http" . "localhost:8339")))
+  ;; 3●install-elisp の関数を利用可能にする
+  (auto-install-compatibility-setup))   ; 4●
+
+;; ▼要拡張機能インストール▼
+;;; P114-115 auto-installを利用する
+;; (install-elisp "http://www.emacswiki.org/emacs/download/redo+.el")
+(when (require 'redo+ nil t)
+  ;; C-' にリドゥを割り当てる
+  (global-set-key (kbd "C-'") 'redo)
+  ;; 日本語キーボードの場合C-. などがよいかも
+  ;; (global-set-key (kbd "C-.") 'redo)
+  ) ; ←ここでC-x C-eで設定反映
+
+;; ▼要拡張機能インストール▼（ただし、Emacs24からはインストール不要）
+;;; P115-116 Emacs Lisp Package Archive（ELPA）──Emacs Lispパッケージマネージャ
+;; package.elの設定
+(when (require 'package nil t)
+  ;; パッケージリポジトリにMarmaladeと開発者運営のELPAを追加
+;  (add-to-list 'package-archives
+;               '("marmalade" . "http://marmalade-repo.org/packages/"))
+;  (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)  
+  ;; インストールしたパッケージにロードパスを通して読み込む
+  (package-initialize))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 6.2 統一したインタフェースでの操作                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ▼要拡張機能インストール▼
+;;; P122-129 候補選択型インタフェース──Anything
+;; (auto-install-batch "anything")
+(when (require 'anything nil t)
+  (setq
+   ;; 候補を表示するまでの時間。デフォルトは0.5
+   anything-idle-delay 0.3
+   ;; タイプして再描写するまでの時間。デフォルトは0.1
+   anything-input-idle-delay 0.2
+   ;; 候補の最大表示数。デフォルトは50
+   anything-candidate-number-limit 100
+   ;; 候補が多いときに体感速度を早くする
+   anything-quick-update t
+   ;; 候補選択ショートカットをアルファベットに
+   anything-enable-shortcuts 'alphabet)
+
+  (when (require 'anything-config nil t)
+    ;; root権限でアクションを実行するときのコマンド
+    ;; デフォルトは"su"
+    (setq anything-su-or-sudo "sudo"))
+
+  (require 'anything-match-plugin nil t)
+
+  (when (and (executable-find "cmigemo")
+             (require 'migemo nil t))
+    (require 'anything-migemo nil t))
+
+  (when (require 'anything-complete nil t)
+    ;; lispシンボルの補完候補の再検索時間
+    (anything-lisp-complete-symbol-set-timer 150))
+
+  (require 'anything-show-completion nil t)
+
+  (when (require 'auto-install nil t)
+    (require 'anything-auto-install nil t))
+
+  (when (require 'descbinds-anything nil t)
+    ;; describe-bindingsをAnythingに置き換える
+    (descbinds-anything-install)))
+
+;; ▼要拡張機能インストール▼
+;;; P127-128 過去の履歴からペースト──anything-show-kill-ring
+;; M-yにanything-show-kill-ringを割り当てる
+(define-key global-map (kbd "M-y") 'anything-show-kill-ring)
+
+;; ▼要拡張機能インストール▼
+;;; P128-129 moccurを利用する──anything-c-moccur
+;(when (require 'anything-c-moccur nil t)
+;  (setq
+   ;; anything-c-moccur用 `anything-idle-delay'
+;   anything-c-moccur-anything-idle-delay 0.1
+   ;; バッファの情報をハイライトする
+;   anything-c-moccur-higligt-info-line-flag t
+   ;; 現在選択中の候補の位置をほかのwindowに表示する
+;   anything-c-moccur-enable-auto-look-flag t
+   ;; 起動時にポイントの位置の単語を初期パターンにする
+;   anything-c-moccur-enable-initial-pattern t)
+  ;; C-M-oにanything-c-moccur-occur-by-moccurを割り当てる
+;  (global-set-key (kbd "C-M-o") 'anything-c-moccur-occur-by-moccur))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 6.3 入力の効率化                                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ▼要拡張機能インストール▼
+;;; P130-131 利用可能にする
+(when (require 'auto-complete-config nil t)
+  (add-to-list 'ac-dictionary-directories 
+    "~/.emacs.d/elisp/ac-dict")
+  (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+  (ac-config-default))
+
+
+
+;;backup 
+;;github http://wp.yat-net.com/?p=3874
+;;dropbox http://succi.jp/blog/?p=92
+
+
